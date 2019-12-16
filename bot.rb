878107258cdc7a3807cea57c2ffdc6497cfbef46
Alherd Byk <bykalgerd@gmail.com>
2019-12-16 14:19:03 +0300

    made desc full word

--- bot.rb
+++ bot.rb
@@ -7,7 +7,7 @@ module RubyBuild
             command :get_latest_episode do
                 title 'get_latest_episode'
                 desc 'description non classic'
-                long_desc 'long_desc 19'
+                long_desc 'long_description 19'
             end
         end
     end
