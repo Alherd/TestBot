b9fd18e49455f18e0e46db9d61a6d1859f03cce8
Alherd Byk <bykalgerd@gmail.com>
2019-12-16 14:18:45 +0300

    male 19 desc

--- bot.rb
+++ bot.rb
@@ -7,7 +7,7 @@ module RubyBuild
             command :get_latest_episode do
                 title 'get_latest_episode'
                 desc 'description non classic'
-                long_desc 'long_desc 18'
+                long_desc 'long_desc 19'
             end
         end
     end
