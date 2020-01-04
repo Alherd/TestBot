6b9a8936439da9631d74c8c3eee7f33ae803a54d
Alherd Byk <bykalgerd@gmail.com>
2020-01-04 18:28:33 +0300

    description last 60

--- bot.rb
+++ bot.rb
@@ -8,7 +8,7 @@ module RubyBuild
                 title 'get latest release'
                 desc 'description non classic'
                 desc2 'description non classic'
-                long_desc 'long_description 50'
+                long_desc 'long_description 60'
             end
         end
     end
