6d4517413a94fb00d003ce7af7b5ddd57c2b2ab7
Alherd Byk <bykalgerd@gmail.com>
2020-02-07 12:46:54 +0300

    another one

--- bot.rb
+++ bot.rb
@@ -7,7 +7,7 @@ module RubyBuild
             command :get_latest_release do
                 title 'get latest release'
                 desc 'description non classic'
-                desc2 'description non classiqq'
+                desc2 'description non classiq'
                 long_desc 'long_description 100'
             end
         end
